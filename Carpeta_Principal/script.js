a4f5576d988b5c003f57f118c39c8c9322e93ea4
Benjamin-gutierrez <benja.gutierrezvillegas@gmail.com>
2025-07-02 19:03:55 -0500

    ajustes de proyecto

---
+++ Carpeta_Principal/script.js
@@ -0,0 +1,11 @@
+function saludarWhatsapp() {
+    alert("Has abierto el enlace al whatsapp del restaurante");
+}
+
+function saludarTiktok() {
+    alert("Has abierto el enlace al tik tok del restaurante");
+}
+
+function saludarInstagram() {
+    alert("Has abierto el enlace al instagram del restaurante");
+}
